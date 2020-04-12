9d237d2178b8c1d98e341c0cf51f9c970ea92a74
dario <darioh.dev@gmail.com>
2020-04-12 00:29:44 -0300

    add colors

--- src/BoxContainer.js
+++ src/BoxContainer.js
@@ -5,7 +5,20 @@ import './boxContainer.css';
 class BoxContainer extends Component {
     static defaultProps = {
         numBoxes: 16,
-        allColors: ["purple", "magenta", "pink", "red"]
+        allColors: [
+            '#BA68C8',
+            '#9575CD',
+            '#7986CB',
+            '#9C27B0',
+            '#673AB7',
+            '#3F51B5',
+            '#7B1FA2',
+            '#512DA8',
+            '#303F9F',
+            '#E91E63',
+            '#F06292',
+            '#C2185B'
+        ]
     }
 
     render() {
