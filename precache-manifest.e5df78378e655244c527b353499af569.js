91724ec91aaddce9ed59dcb25a0928f3e736eabd
dario <darioh.dev@gmail.com>
2020-04-23 14:07:45 -0300

    Updates

---
+++ precache-manifest.e5df78378e655244c527b353499af569.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47dc288571647fc014c12610efc18f8d",
+    "url": "/React-color-boxes/index.html"
+  },
+  {
+    "revision": "c6ba8ff2025d86fc6e52",
+    "url": "/React-color-boxes/static/css/main.c237722a.chunk.css"
+  },
+  {
+    "revision": "0d3ecee49eeb7bd80b20",
+    "url": "/React-color-boxes/static/js/2.d374d1e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-color-boxes/static/js/2.d374d1e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6ba8ff2025d86fc6e52",
+    "url": "/React-color-boxes/static/js/main.b5482b29.chunk.js"
+  },
+  {
+    "revision": "0066699ad5b87946bc4e",
+    "url": "/React-color-boxes/static/js/runtime-main.6b07da0d.js"
+  }
+]);
